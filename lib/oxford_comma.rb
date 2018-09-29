12a98127210f3465086e94e9973fe3d33ba54ca3
Ana Harris <vana1990@gmail.com>
2018-09-29 19:16:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size <= 1 
+    return array.join
+    elsif array.size == 2 
+    return "#{array.first} and #{array.last}"
+  else
+    last_element = array.pop
+    list = array.join(", ")
+    return list + ", and #{last_element}"
+  end
 end
